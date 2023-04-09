af8b4380addbbb89c821acc0cd36aa26aed93e2c
unknown <doniaskima344@gmail.com>
2023-04-09 07:11:34 +0100

    fix Join Page

--- client/src/pages/Join.jsx
+++ client/src/pages/Join.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import { SocketContext } from '../SocketContext';
 import { useNavigate } from 'react-router-dom';
 import Spinner from "../components/Spinner";
@@ -9,7 +9,6 @@ import VideocamOffIcon from '@material-ui/icons/VideocamOff';
 import { message } from 'antd';
 
 const Join = () => {
-
   const {
     socketState: socket,
     name,
@@ -27,121 +26,140 @@ const Join = () => {
     otherUser,
     showVideoToOtherUser
   } = useContext(SocketContext);
-
-  //reference to my video 
+  const bgColor = 'bg-base-300';
+  //reference for my video
   const myPreviewVideo = useRef();
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!newMeet && meetingCode.length === 0) {
+      navigate('/');
+      window.location.reload();
+      return;
+    }
+    //to get the video and audio
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((res) => {
+        res.getAudioTracks()[0].enabled = false;
+        myPreviewVideo.current.srcObject = res;
+      });
+  }, []);
+
   return (
     <>
-      <div className="flexbox-join-page">
-        <div className="flexbox-join-enter-button">
+      
+      <div className='flexbox-join-page'>
+        <div className="flexbox-join-enter-button ">
           <h1 className="mb-8">Your video appears like this</h1>
-
-            {/* video preview */}
-            <div className="video-div">
-              {
-                stream ? (
-                  <video 
-                  src=""
-                  ref={myPreviewVideo}
-                  autoplay
-                  muted
-                  >
-                  </video>
-                ) : (
-                  <Spinner />
-                )
-              }
-            </div>
-
-            <div className="flexbox-control-video">
-              {/* video controls (mute/unmute, video off/video on) */}
-              <button 
-                  onClick={()=>updateMicStatus()}
+          
+          
+          {/* video preview */}
+          <div className='video-div'>
+            {stream ? (
+              <video
+                src=''
+                ref={myPreviewVideo}
+                autoPlay
+                muted
+              ></video>
+            ) : (
+              <Spinner />
+            )}
+          </div>
+          {/* video controls (mute/unmute, video off/video on) */}
+          <div className="flexbox-control-aud-vid">
+                <button
+                  onClick={() => updateMicStatus()}
+                  type='primary'
                   className={!myMicStatus ? 'bg-grey tooltip rounded-xl px-2 py-1 mr-2' : 'bg-white tooltip rounded-xl px-2 py-1 mr-2'}
-              >
+                >
                    {' '}
-                   {myMicStatus ? <MicIcon /> : <MicOffIcon />}
-              </button>
-              <button 
-               onClick={()=>updateVideoStatus()}
-              >
-              {myVideoStatus ? <VideocamIcon /> : <VideocamOffIcon />}
-              </button>
-            </div>
+              {myMicStatus ? <MicIcon /> : <MicOffIcon />}
+             
+                  
+                </button>
+                <button
+
+                  onClick={() => updateVideoStatus()}
+                    className={!myVideoStatus ? 'bg-grey tooltip rounded-xl px-2 py-1' : 'bg-white tooltip rounded-xl px-2 py-1'}
+                >
+                  {myVideoStatus ? <VideocamIcon /> : <VideocamOffIcon />}
+                
+                </button>
+              </div>
         </div>
-        {
-          stream && (
-            <>
-             //Name Input 
-             <div className="flexbox-join-enter-button">
-              <div className="flexbox-join-enter">
-                <label htmlFor="field field_v1">
-                  <input 
-                    className="field__input"
+        {/* name input */}
+        {stream && (
+          <>
+            <div className="flexbox-join-enter-button">
+              <div className={`flexbox-join-enter ${bgColor}`}>
+                <label class="field field_v1 text-white" >
+                  <input
+                    className= "py-4 text-black"
                     id="html"
                     type='text'
                     placeholder='Enter your name'
                     value={name}
                     onChange={(e) => {
-                        setName(e.target.value);
+                      setName(e.target.value);
                     }}
                   />
-                    <span class="field__label-wrap">
-                    <span class="field__label">Name</span>
-                  </span>
+              
                 </label>
-                    //show start for the host and join for others 
-                    {
-                      newMeet ? (
-                        <button 
-                        onClick={()=>{
-                          if(name.trim().length===0){
-                            message.error("Enter your name");
-                            return;
-                          }
-                          if(otherUser) showVideoToOtherUser();
-                          console.log(otherUser);
-                          navigate("/meet");
-                        }}
-                        className='font-sans chatroom-meeting-btn'>
-                          Start
-                        </button>
-                      ) : (
-                        <button
-                        className='chatroom-meeting-btn font-sans'
-                        onClick={() => {
-                          if (name.trim().length === 0) {
-                            message.error('Please enter your name');
-                            
-                            return;
-                          }
-                          if(otherUser) showVideoToOtherUser();
-                          callUser(meetingCode);
-                          navigate('/meet');
-                        }}
-                      >
-                        Join
-                      </button>
-                      )
-                    }
 
-                    <button 
-                        className='chatroom-meeting-btn font-sans' 
-                        onClick={() => {
-                            navigate('/chatRoom');
-                        }}
-                    >
-                      Leave 
-                    </button>
+                {/* show start for the host and join for others */}
+                {newMeet ? (
+                  <button
+                    className='bg-blue-500 text-white font-semibold px-5 py-1 rounded-full shadow-md'
+                    onClick={() => {
+                      if (name.trim().length === 0) {
+                        message.error("Enter your Name!");
+      
+                        return;
+                      }
+                      if(otherUser) showVideoToOtherUser();
+                      console.log(otherUser);
+                      navigate('/meet');
+                    }}
+                  >
+                    Start
+                  </button>
+                ) : (
+                  <button
+                    className='bg-blue-500 text-white font-semibold px-5 py-1 rounded-full shadow-md'
+                    onClick={() => {
+                      if (name.trim().length === 0) {
+                        message.error('Please enter your name');
+                        
+                        return;
+                      }
+                      if(otherUser) showVideoToOtherUser();
+                      callUser(meetingCode);
+                      navigate('/meet');
+                    }}
+                  >
+                    Join
+                  </button>
+                )}
+                <button
+                  className='bg-blue-500 text-white font-semibold px-5 py-1 rounded-full shadow-md' 
+                  onClick={() => {
+                    navigate('/chatRoom');
+                  }}
+                >
+                  Leave
+                </button>
               </div>
-             </div>
-            </>
-          )
-        }
+            </div>
+          </>
+
+        )}
+
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Join
+export default Join;
